ccda981d6e1fe1feee4a7e9f672e1ac3f695cdc1
rudy <doomdexter07@gmail.com>
2022-11-08 09:25:56 +0530

    added report adding feature

---
+++ src/DTO/createReport.dto.ts
@@ -0,0 +1,37 @@
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Min,
+  Max,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1999)
+  @Max(2022)
+  year: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+}
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Get,Post,UseGuards } from '@nestjs/common';
+import {CreateReportDto} from '../DTO/createReport.dto'
+import { ReportsService } from './reports.service';
+import { AuthGuard } from './../guards/auth-guard';
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    constructor(private reportService:ReportsService){}
+    
+    @Post()
+    @UseGuards(AuthGuard)
+    createReport(@Body() body:CreateReportDto){
+        return this.reportService.create(body)
+    }
+}
