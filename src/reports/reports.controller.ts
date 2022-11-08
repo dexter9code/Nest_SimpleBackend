0a06ab9013f979a8349a985a3718eee789c9cd35
rudy <doomdexter07@gmail.com>
2022-11-08 17:43:00 +0530

    added relationship b/t tables

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,13 +2,19 @@ import { Body, Controller, Get,Post,UseGuards } from '@nestjs/common';
 import {CreateReportDto} from '../DTO/createReport.dto'
 import { ReportsService } from './reports.service';
 import { AuthGuard } from './../guards/auth-guard';
+import { CurrentUser } from './../decorators/current-user.decorator';
+import { User } from './../users/user.entity';
+import { Serialize } from './../interceptors/serialize.interceptor';
+import { ReportDto } from './../DTO/report.dto';
+
 @Controller('reports')
 export class ReportsController {
     constructor(private reportService:ReportsService){}
     
     @Post()
     @UseGuards(AuthGuard)
-    createReport(@Body() body:CreateReportDto){
-        return this.reportService.create(body)
+    @Serialize(ReportDto)
+    createReport(@Body() body:CreateReportDto,@CurrentUser() user:User){
+        return this.reportService.create(body,user)
     }
 }
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,12 @@
-import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterRemove } from 'typeorm';
+import { Report } from './../reports/report.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterInsert,
+  AfterRemove,
+  OneToMany,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -11,13 +19,16 @@ export class User {
   @Column()
   password: string;
 
+  @OneToMany(()=>Report,(report)=>report.user)  //1.what of assiocation is ex report and its wrap around the function because of circular dependcy at that time Report entity is not load up therefore we need callBack function
+  repots:Report[]
+
   @AfterInsert() //hooks which run after instance of entity is created
-  logInsert(){
-    console.log(`Inserted user Id ${this.id}`)
+  logInsert() {
+    console.log(`Inserted user Id ${this.id}`);
   }
 
   @AfterRemove()
-  logDelete(){
-    console.log(`Deleted User Id ${this.id}`)
+  logDelete() {
+    console.log(`Deleted User Id ${this.id}`);
   }
 }
